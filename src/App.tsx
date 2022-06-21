78ea77232e5fe654b6a4776929c6d5f624932657
fadhilsaheer <fadhilsaheer@gmail.com>
2022-06-21 16:18:35 +0530

    social

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { Avatar, Center, Container, Heading, Text } from '@chakra-ui/react';
+import { Container, Heading, Text } from '@chakra-ui/react';
 import { Header } from './components/Header';
 import { getAge } from './utils/helper';
 import { Profile } from './components/Profile';
+import { Socials } from './components/Socials';
 
 export const App: React.FC = () => {
   return (
     <Container>
       <Header />
       <Profile />
-      <Heading mt={10} className='text-2xl'>
+      <Heading mt={14} className='text-2xl'>
         Fadhil Saheer⚡
       </Heading>
-      <Text mt={2}>
+      <Text className='text-lg' mt={2}>
         Professional idiot, {getAge()} y/o, Passionate developer.
       </Text>
+      <Socials />
     </Container>
   );
 };
