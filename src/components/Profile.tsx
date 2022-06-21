40f1d0bc2339352a88f7744739ca5fc134f27814
fadhilsaheer <fadhilsaheer@gmail.com>
2022-06-21 16:52:05 +0530

    refactor

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Avatar, Center } from '@chakra-ui/react';
+import ComputerImage from '../computer.jpg';
+import MeImage from '../me.jpg';
 
 export const Profile: React.FC = () => {
   return (
     <div className='relative'>
       <img
-        src='/assets/computer.jpg'
+        src={ComputerImage}
         alt='Wall'
         className='w-full h-40 object-cover mt-5 rounded'
       />
       <Center>
         <Avatar
-          src='/assets/me.jpg'
+          src={MeImage}
           size='xl'
           className='absolute border-4 border-teal-400'
           name='Fadhil'
