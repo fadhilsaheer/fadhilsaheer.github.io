ec1a7a41dfc2256a1a6b2b1f988998e5394a6d54
Fadhil <fadhilsaheer@gmail.com>
2022-08-04 23:53:39 -0700

    skills

---
+++ src/data/skills.ts
@@ -0,0 +1,71 @@
+interface SkillType {
+  title: string;
+  icon: string;
+}
+
+export const skills: SkillType[] = [
+  {
+    title: 'Typescript',
+    icon: 'https://s2.svgbox.net/files.svg?ic=typescript-official',
+  },
+  {
+    title: 'Rust',
+    icon: 'https://s2.svgbox.net/files.svg?ic=rust',
+  },
+  {
+    title: 'Dart',
+    icon: 'https://s2.svgbox.net/files.svg?ic=dartlang',
+  },
+  {
+    title: 'Golang',
+    icon: 'https://s2.svgbox.net/files.svg?ic=go-gopher',
+  },
+  {
+    title: 'React',
+    icon: 'https://s2.svgbox.net/files.svg?ic=reactjs',
+  },
+  {
+    title: 'Next JS',
+    icon: 'https://s2.svgbox.net/files.svg?ic=next',
+  },
+  {
+    title: 'Graphql',
+    icon: 'https://s2.svgbox.net/files.svg?ic=graphql',
+  },
+  {
+    title: 'Node JS',
+    icon: 'https://s2.svgbox.net/files.svg?ic=node',
+  },
+  {
+    title: 'Prisma',
+    icon: 'https://s2.svgbox.net/files.svg?ic=prisma',
+  },
+  {
+    title: 'Flutter',
+    icon: 'https://s2.svgbox.net/files.svg?ic=flutter',
+  },
+  {
+    title: 'Postgres',
+    icon: 'https://s2.svgbox.net/files.svg?ic=pgsql',
+  },
+  {
+    title: 'SQlite',
+    icon: 'https://s2.svgbox.net/files.svg?ic=sqlite',
+  },
+  {
+    title: 'Mongo',
+    icon: 'https://s2.svgbox.net/files.svg?ic=mongo',
+  },
+  {
+    title: 'Tailwind',
+    icon: 'https://s2.svgbox.net/files.svg?ic=tailwind',
+  },
+  {
+    title: 'Git',
+    icon: 'https://s2.svgbox.net/files.svg?ic=git',
+  },
+  {
+    title: 'Docker',
+    icon: 'https://s2.svgbox.net/files.svg?ic=docker2',
+  },
+];
