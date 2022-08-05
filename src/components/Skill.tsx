118f88aff12965ec42acf36c55ebe3653eb2e2d6
Fadhil <fadhilsaheer@gmail.com>
2022-08-04 23:53:15 -0700

    skill

---
+++ src/components/Skill.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+interface Props {
+  icon: string;
+  title: string;
+}
+
+export const Skill: React.FC<Props> = ({ icon, title }) => {
+  return (
+    <div className='cursor-pointer flex items-center overflow-hidden rounded-2xl bg-white/5 py-4 px-3 shadow-surface-elevation-low transition duration-300 hover:bg-white/10 hover:shadow-surface-elevation-medium'>
+      <img src={icon + '&color=000'} alt={title} className='w-7' />
+      <p className='ml-2 text-xl'>{title}</p>
+    </div>
+  );
+};
