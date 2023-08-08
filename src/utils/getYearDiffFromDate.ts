bb500d1296d65284e0ba6c627c03ad321b496e8a
Fadhil <fadhilsaheer@gmail.com>
2023-08-08 20:22:28 +0530

    removed old files

--- src/utils/getYearDiffFromDate.ts
+++
@@ -1,7 +0,0 @@
-export const getYearDiffFromDate = (date: string) => {
-  // Copied from `https://javatpoint.com/calculate-age-using-javascript`
-  const dateTime = new Date(date);
-  return Math.abs(
-    new Date(Date.now() - dateTime.getTime()).getUTCFullYear() - 1970
-  );
-};
