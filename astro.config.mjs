8519ca20054210de7946fbb4c8292b3d3eb5a3f6
fadhilsaheer <fadhilsaheer@gmail.com>
2025-11-13 06:39:31 +0530

    setup: tailwind & port, disabled dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  server: { port: 3000 },
+  devToolbar: { enabled: false },
+  vite: {
+    plugins: [tailwindcss()],
+  },
+});
